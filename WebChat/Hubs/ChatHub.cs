9644dc46a7cd3816ed620afb49a26d77716ff716
Jai <jaikumar.bhagwani@gmail.com>
2019-09-26 13:35:34 +0530

    Initial commit

---
+++ WebChat/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebChat.Hubs
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void LetsChat(string Cl_Name, string Cl_Message)
+        {
+            Clients.All.NewMessage(Cl_Name, Cl_Message);
+        }
+    }
+}
---
+++ WebChat/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Owin;
+
+namespace WebChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.MapSignalR();
+        }
+
+    }
+}
